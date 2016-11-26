0c8c7d307ba3f6e192e97d04fde81ad5eefbe3f8
parrottq <quinn.parrott@gmail.com>
2016-11-25 22:19:47 -0600

    Removed lftp.py

--- lftp.py
+++
@@ -1,8 +0,0 @@
-from subprocess import run, PIPE
-from time import sleep
-
-
-def get_files(server):
-    lftp_process = run(["lftp", "-e", "cls -1; exit", "http://mirror.js-webcoding.de/pub/archlinux/core/os/x86_64/"], stdout=PIPE)
-    return lftp_process.stdout.decode().split("\n")
-
