cc72d4042275241bfc7c58875589ff7e0290edd0
parrottq <quinn.parrott@gmail.com>
2016-11-28 20:41:45 -0600

    Implemented progress bar.

--- download.py
+++ download.py
@@ -1,5 +1,15 @@
 import requests
 from time import time
+from shutil import get_terminal_size
+from math import ceil
+
+
+colours = {
+        "reset"  : "\x1b[0m",
+        "green"  : "\x1b[32m",
+        "yellow" : "\x1b[33m",
+}
+
 
 def download_package(url):
     download = requests.get(url, stream=True)
@@ -9,10 +19,35 @@ def download_package(url):
         for chunk in download.iter_content(chunk_size=1024):
             if chunk:
                 ti += 1
-                if ti % 100 == 0:
-                    yield ti
+                yield ti
                 f.write(chunk)
         yield ti
+
+
+def create_progress_bar(progress):
+    t_size = get_terminal_size().columns
+
+    start ="["
+    end = "] {0}{1}KB/{2}KB".format(" " * (len(str(progress[2])) - len(str(progress[0]+progress[1]))), int((progress[0]+progress[1])/1000), int(progress[2]/1000))
+    len_bar = t_size - len(start+end)
+    conversion = len_bar / progress[2]
+
+
+    body = []
+    body.append("".join(["#" for e in range(int(progress[1] * conversion))]))
+    body.append("".join(["#" for e in range(int(progress[0] * conversion))]))
+    body.append("".join(["-" for e in range(len_bar - len("".join(body)))]))
+
+    body_colour = [
+        colours["green"], # Colour of finished package sizes
+        colours["yellow"], # Colour of unfinished package sizes
+        colours["reset"], # Resets colour
+    ]
+    progress = ""
+    for b in zip(body_colour, body):
+        progress += "".join(b)
+
+    return start + progress + end
 
 
 if __name__ == "__main__":
--- thread.py
+++ thread.py
@@ -1,5 +1,5 @@
 import threading
-from download import download_package
+from download import *
 import check
 from time import sleep
 
@@ -30,7 +30,7 @@ class DownloadManager:
         size = 0
         for s in self.threads:
             size += s.size
-        self.total_size = size
+        self.all_size = size
         self.done_size = 0
         self.removed_size = 0
 
@@ -56,19 +56,21 @@ class DownloadManager:
             self.done_size = 0
             for t in self.running:
                 self.done_size += t.progress * 1000
-            self.done_size += self.removed_size
 
-            yield (self.done_size, self.total_size)
+            yield (self.done_size, self.removed_size, self.all_size)
             sleep(0.1)
         print()
 
 
 if __name__ =="__main__":
     import files
+    print("\rFetching mirrors...", end="")
     mirrors = check.get_mirrors()
-    packages = check.match_packages(check.get_package_size(files.f, mirrors[0]), mirrors)
+    print("\rMatching package with mirror...", end="")
+    packages = check.match_packages(check.get_package_size(check.get_updates(), mirrors[0]), mirrors)
 
     dm = DownloadManager(packages)
     for done in dm.start():
-        print(done)
+        print("\r" + create_progress_bar(done), end="")
+    print()
 
