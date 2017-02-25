a0abd2b12fc2c9ed83bf6f8259c1c66fd33a6235
parrottq <quinn.parrott@gmail.com>
2017-02-24 21:20:53 -0600

    Outline for mirror.py

---
+++ fpd_lib/mirror.py
@@ -0,0 +1,49 @@
+
+
+
+class Mirrors:
+    """
+    A collection of methods related to mirror manipulation
+    """
+
+    mirrors = []
+
+
+    @staticmethod
+    def search(package):
+        """
+        Search for a package in the mirror list
+
+        Note:
+        Returns a generator that keeps adding and supplying mirrors
+
+
+        Keyword arguments:
+        
+        package -- a package object
+
+        """
+        pass
+
+
+    @staticmethod
+    def find():
+        """
+        Finds the next highest rated mirror
+        """
+        pass
+
+
+    @staticmethod
+    def add(mirror):
+        """
+        Downloads and decompresses a mirror
+
+
+        Keyword arguements:
+
+        mirror -- base mirror url
+
+        """
+        pass
+
